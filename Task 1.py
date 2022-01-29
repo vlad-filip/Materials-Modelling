a201d48585bcbf6179913514142ffd099fe72726
Vlad Filip <vf278@cam.ac.uk>
2022-01-29 21:11:36 +0000

    imports

--- Task 1.py
+++ Task 1.py
@@ -1,1 +1,13 @@
+from __future__ import print_function
 import numpy as np
+from ase import Atoms
+from ase.units import eV, Ang, GPa
+d = 2.5*Ang
+a = Atoms('2Cu', positions=[(0., 0., 0.), (0., 0., d)])
+import sys
+sys.path.append(r'files')
+import Morse
+calc = Morse.MorsePotential()
+
+a.set_calculator(calc) 
+a.get_potential_energy()
